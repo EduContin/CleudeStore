c32b7c30f47635a76eb0b6d2d13c7c9ab67966be
Vitor Nicoletti <128414008+vitorNicoletti@users.noreply.github.com>
2023-10-30 16:03:19 -0300

    Update novoproduto.php

--- php/novoproduto.php
+++ php/novoproduto.php
@@ -1,32 +1,37 @@
 <?php 
-$arquivo = $_FILES["arquivo"];
+if (isset($_FILES["arquivo"])) {
+    $arquivo = $_FILES["arquivo"];
 
+    $type =$arquivo["type"];
+    $nome = $_POST["nome"];
+    $preco = $_POST["preco"];
 
-$type =$arquivo["type"];
-$nome = $_POST["nome"];
-$preco = $_POST["preco"];
+    # checagem da extensao da imagem e ve se os campos foram mandados nulos
+    if($type == "image/png" && $nome != NULL && $preco != NULL){
+        $con = mysqli_connect("localhost:3306", "root", "SERVER1234", "cleude");
 
-# checagem da extensao da imagem e ve se os campos forma mandados nulos
-if($type == "image/png" && $nome != NULL && $preco != NULL){
-    $con = mysqli_connect("localhost:3306", "root", "root", "cleude");
+        $query = "INSERT INTO produtos(nome,preco) VALUES ('" . $nome . "', '" . $preco . "')";
+        mysqli_query($con,$query);
 
-    $query = "INSERT INTO produtos(nome,preco) VALUES ('" . $nome . "', '" . $preco . "')";
-    mysqli_query($con,$query);
+        #pega o id do produto inserido 
+        $last_id = $con->insert_id;
 
-    #pega o id do produto inserido 
-    $last_id = $con->insert_id;
+        #define o nome do png para seu valor de id
+        $novo_endereco = "../img/produtos/".$last_id . ".png";
+        move_uploaded_file($arquivo["tmp_name"],$novo_endereco);
 
-    #define o nome do png para seu valor de id
-    $novo_endereco = "../img/produtos/".$last_id . ".png";
-    move_uploaded_file($arquivo["tmp_name"],$novo_endereco);
-
-    #feedback para o usuario
-    $msg = "Dados adicionados com sucesso!";
+        #feedback para o usuario
+        $msg = "Dados adicionados com sucesso!";
+    }
+    else{
+        #Feedback para o usuario
+        $msg = "ERRO: tipo de arquivo de imagem incorreto ou algum campo está vazio.";
+    }
+} 
+else
+{
+    $msg = "ERRO: coloque um arquivo para imagem(.png)";
 }
-else{
-    #Feedback para o usuario
-    $msg = "ERRO: tipo de arquivo de imagem incorreto ou algum campo está vazio.";
-}
-echo json_encode($msg);
 
+    echo json_encode($msg);
 ?>
