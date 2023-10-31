13411c3d9bdab6640b6195d50c1239fa04bd2f98
Vitor Nicoletti <128414008+vitorNicoletti@users.noreply.github.com>
2023-10-31 19:41:07 -0300

    Update getcarrinho.php

--- php/getcarrinho.php
+++ php/getcarrinho.php
@@ -2,8 +2,8 @@
 
     $con = mysqli_connect("localhost:3306", "root", "root", "cleude");
 
-    $resultado = mysqli_query($con, "SELECT p.id, p.nome, p.preco FROM produtos AS p INNER JOIN carrinho as c ON c.id_produto = p.id");
-
+    $resultado = mysqli_query($con, "SELECT p.id, p.nome, p.preco, c.quantidade FROM produtos AS p INNER JOIN carrinho as c ON c.id_produto = p.id");
+    
     $dados = array();
 
     while($registro = mysqli_fetch_assoc($resultado)){
