430b23595a5a6cf196426d58371668436be7196b
Vitor Nicoletti <128414008+vitorNicoletti@users.noreply.github.com>
2023-10-31 18:26:42 -0300

    Update removerproduto.php

--- php/removerproduto.php
+++ php/removerproduto.php
@@ -6,6 +6,9 @@
     print_r($id);
 
     $con = mysqli_connect("localhost:3306", "root", "root", "cleude");
+    $query = "DELETE FROM carrinho WHERE id_produto = " . $id;
+    $resultado = mysqli_query($con,$query);
+    $query = "DELETE FROM produtos WHERE id = ". $id;
+    $resultado = mysqli_query($con,$query);
     
-    $resultado = mysqli_query($con, "DELETE FROM produtos WHERE id = $id");
 ?>
